ef26f991707bbc25f90f2f143390cc1bb5344652
fakecodes <dsd010216@gmail.com>
2024-01-12 00:23:26 +0700

    kendaraan

---
+++ kendaraan/README.md
@@ -0,0 +1,25 @@
+
+# Team Assignment
+
+The following source code is used as an answer to a group assignment in the object oriented programming course.
+
+## Question
+Kelas dasar adalah Kendaraan. Kelas turunan dari kelas Kendaraan adalah kelas Mobil, kelas Motor dan kelas Bajaj.  Pada kelas Kendaraan terdapat informasi mengenai nama kendaraan dan jumlah roda kendaraan. Informasi nama dan jumlah kendaraan dapat diperoleh dari kelas turunannya.  Buatlah program tersebut menggunakan konsep polymorphism dengan fungsi pure virtual ! Contoh output:
+
+aturan keluaran sebagai berikut:
+```
+Informasi Kendaraan
+Nama   = Mobil
+Jumlah Roda    = 4 Buah
+
+Informasi Kendaraan
+Nama   = Motor 
+Jumlah Roda    = 2 Buah
+
+Informasi Kendaraan
+Nama   = Bajaj
+Jumlah Roda    = 3 Buah
+```
+
+## Output of the Answer 
+![Screenshot](screenshot-output.png)
---
+++ kendaraan/kendaraan.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+using namespace std;
+
+class Kendaraan {
+public:
+    virtual void PrintInfo() {
+        cout << "Informasi Kendaraan" << endl;
+        cout << "Nama\t\t: Mobil" << endl;
+        cout << "Jumlah Roda\t: 4 Buah" << endl << endl;
+    }
+};
+
+class Mobil : public Kendaraan {
+public:
+
+};
+
+class Motor : public Kendaraan {
+    public:
+    virtual void PrintInfo() {
+        cout << "Informasi Kendaraan" << endl;
+        cout << "Nama\t\t: Motor" << endl;
+        cout << "Jumlah Roda\t: 2 Buah" << endl << endl;
+    }
+};
+
+class Bajaj : public Kendaraan {
+    public:
+    virtual void PrintInfo() {
+        cout << "Informasi Kendaraan" << endl;
+        cout << "Nama\t\t: Bajaj" << endl;
+        cout << "Jumlah Roda\t: 3 Buah" << endl << endl;
+    }
+};
+
+int main() {
+    Kendaraan* mobil = new Mobil;
+    Kendaraan* motor = new Motor;
+    Kendaraan* bajaj = new Bajaj;
+
+    Kendaraan* kendaraan[3] = {mobil, motor, bajaj};
+    for (int i=0; i<3; i++) {
+        kendaraan[i]->PrintInfo();
+    }
+
+    return 0;
+}
